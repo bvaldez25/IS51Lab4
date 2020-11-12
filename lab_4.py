065d8a4cd2c98532c3cd218633ff2081976ad15b
bvaldez25 <bvaldez25@mail.fresnostate.edu>
2020-11-11 19:36:10 -0800

    finish psuedocode

--- lab_4.py
+++ lab_4.py
@@ -17,3 +17,23 @@ If the user could not guess the correct answer within the max_tries, then print
 """
 
 
+"""
+main
+    question = "What is the capital of California"
+    answer = "California"
+    ask(question, answer)
+
+ask
+    tries = 0
+    loop three times
+        increment tries by 1 
+        ask user input()
+        check to see if user input is equal to answer
+            if so, print "correct" then exit loop
+    if not correct 
+        print to the user "You have used up your allotment of guesses".
+        print the correct answer "The correct answer is 'California'"
+
+main
+"""
+
